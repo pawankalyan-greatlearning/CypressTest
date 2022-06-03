00704c3e2b67fefa8ba913799393f7f2634618fd
pawankalyan-greatlearning <pawankalyan.veldurthi@greatlearning.in>
2022-06-03 10:27:58 +0530

    upto 1 level of filter function

---
+++ cypress/e2e/topicsPage.cy.js
@@ -0,0 +1,30 @@
+describe('courses filter', () => {
+    beforeEach(()=>{
+        cy.visit(`https://www.mygreatlearning.com/aws/free-courses`)
+    })
+    it('checking about topic', () => {
+        cy.title().should(`include`, `Free AWS Courses`);
+        cy.get('.description').should(`exist`);
+        cy.get(`.description`).should(`be.visible`);
+    });
+    it('filter results', () => {
+        cy.get(`#course-filter`).should(`exist`);
+        cy.get(`.course-filter-sec #course-filter > div.level.filter > div.level.filter-content label`).each((label)=>{
+            cy.get(label).click()
+            cy.wait(2000)
+             cy.get(label).invoke('text').then((val)=>{
+                cy.get(`#course-count`).invoke(`text`).then(text=>{
+                    if(text>0){
+                        cy.get(`#subject-course-cards > a:nth-child(n) > div > div.course-desc > div.course-info > div:nth-child(2)`).each((a)=>{
+                            cy.get(a).invoke(`text`).then((val2)=>{
+                                //    cy.log(val, val2)
+                                expect(val.trim()).equal(val2.trim())
+                            })
+                        })
+                    }
+                })
+             })
+             cy.get(label).click()
+        })
+    });
+});
